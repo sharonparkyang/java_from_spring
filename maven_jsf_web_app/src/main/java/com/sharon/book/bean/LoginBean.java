505dbfb61e67f26ccc4ccc65acce7e4976bb32df
Sunghee Yang <sharonparkyang@hotmail.com>
2018-10-26 21:17:08 -0400

    Maven JSF Web App v.1

---
+++ maven_jsf_web_app/src/main/java/com/sharon/book/bean/LoginBean.java
@@ -0,0 +1,49 @@
+package com.sharon.book.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="loginBean")
+@RequestScoped
+public class LoginBean {
+	private String userName;
+	private String password;
+	private String errorMsg;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+	public String validate() {
+		if("admin".equals(this.userName) &&
+				"admin".equals(this.password)){
+			errorMsg=null;
+			return "welcome";
+		}
+		else {
+			errorMsg="Invalid user id or password. Please try again!";
+			return null;
+		}
+	}
+	public String reset() {
+		this.userName = null;
+		this.password = null;
+		this.errorMsg = null;
+		return "index";
+	}
+
+}
